c264b4d60a80302fd3aafc465b01ae27d8455745
Serhii Hrybok <sergejgribok@gmail.com>
2024-07-11 11:10:32 +0300

    Init

---
+++ TryRx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive.Linq;
+using TryRx;
+
+/*
+IObservable<long> ticks = Observable.Timer(
+    dueTime: TimeSpan.Zero,
+    period: TimeSpan.FromSeconds(1));
+
+ticks.Subscribe(
+    tick => Console.WriteLine($"Tick {tick}"));
+*/
+
+var numbers = new MySequenceOfNumbers();
+numbers.Subscribe(
+    number => Console.WriteLine($"Received value: {number}"),
+    () => Console.WriteLine("Sequence terminated"));
+
+Console.ReadLine();
